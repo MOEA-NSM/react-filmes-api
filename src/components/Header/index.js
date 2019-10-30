5dee4f50a88c99ec1d37daf1f70a8526ff6e5ace
MOEA-NSM <32080513+MOEA-NSM@users.noreply.github.com>
2019-10-30 14:17:30 -0300

    adicao do botao firebase
    adicao do botao firebase

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,23 @@
-import React, {Component} from 'react';
-import './header.css';
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import "./header.css";
+import { Link } from "react-router-dom";
+import { Icon } from "react-native-elements";
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="header">
-                <Link to="/" >Filmes em cartaz</Link>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="header">
+        <Link to="/firebase">Firebase</Link>
+        <Icon
+          raised
+          name="heartbeat"
+          type="font-awesome"
+          color="#f50"
+          onPress={() => console.log("hello")}
+        />
+        <Link to="/">Filmes em cartaz</Link>
+      </div>
+    );
+  }
 }
 export default Header;
